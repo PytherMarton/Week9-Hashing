cd439106cc2307b86524a906846013a5315d3bc8
Peter Marton <petermarton@Peters-MacBook-Pro.local>
2022-01-18 15:45:50 +0000

    inital commit

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 require("dotenv").config();
+const bcrypt = require("bcrypt");
+const saltRounds = parseInt(process.env.SALT_ROUNDS);
+const password = "my_sup3r_s4f3_p4ssw0rd";
+const password2 = "sdasdsadsadsadsafdfgfgdgfd";
 
-console.log(process.env);
+const main = async() => {
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(password, salt);
+    console.log(await bcrypt.compare(password2, hash));
+}
+
+main();
